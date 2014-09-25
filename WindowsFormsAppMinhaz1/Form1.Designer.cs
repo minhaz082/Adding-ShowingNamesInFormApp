60c9965ab674fbe447e14e75e4c14baeb672ec01
minhaz082 <minhaz082@hotmail.com>
2014-09-25 16:35:17 +0600

    Names Adding App

---
+++ WindowsFormsAppMinhaz1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WindowsFormsAppMinhaz1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameInfoListBox = new System.Windows.Forms.ListBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.countNameLabel = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(107, 25);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(323, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // nameInfoListBox
+            // 
+            this.nameInfoListBox.FormattingEnabled = true;
+            this.nameInfoListBox.Location = new System.Drawing.Point(60, 129);
+            this.nameInfoListBox.Name = "nameInfoListBox";
+            this.nameInfoListBox.Size = new System.Drawing.Size(370, 95);
+            this.nameInfoListBox.TabIndex = 2;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(449, 25);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(415, 59);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(111, 23);
+            this.showAllButton.TabIndex = 4;
+            this.showAllButton.Text = "Show All";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Total Names Added :";
+            // 
+            // countNameLabel
+            // 
+            this.countNameLabel.AutoSize = true;
+            this.countNameLabel.Location = new System.Drawing.Point(170, 64);
+            this.countNameLabel.Name = "countNameLabel";
+            this.countNameLabel.Size = new System.Drawing.Size(0, 13);
+            this.countNameLabel.TabIndex = 6;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(449, 93);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 7;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 294);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.countNameLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.nameInfoListBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "List Practice of Adding 5 Names";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.ListBox nameInfoListBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button showAllButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label countNameLabel;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ WindowsFormsAppMinhaz1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppMinhaz1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            resetButton.Enabled = false;
+        }
+
+        string name ;
+        List<String> employeeName = new List<string>();
+        int maximumNo = 0;
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+                name = nameTextBox.Text;
+                employeeName.Add(name);
+                nameTextBox.Clear();
+                maximumNo++;
+                countNameLabel.Text = maximumNo.ToString();
+  
+                if (maximumNo>= 5)
+                {
+                    ShowListBoxInfo();
+                    addButton.Enabled = false;
+                    resetButton.Enabled = true;
+                    showAllButton.Enabled = false;
+                    countNameLabel.Text = "5";
+                }               
+                
+            }
+
+        private void ShowListBoxInfo()
+        {
+            nameInfoListBox.Items.Clear();
+            for (int i = 0; i < employeeName.Count; i++)
+            {
+                if (employeeName[i] != null)
+                {
+                    nameInfoListBox.Items.Add( (i+1).ToString() + ".  " + employeeName[i]);
+                }
+                
+            }
+            
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            ShowListBoxInfo();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            nameInfoListBox.Items.Clear();
+            nameTextBox.Clear();
+            employeeName = new List<string>();
+            resetButton.Enabled = false;
+            countNameLabel.Text = "";
+            maximumNo = 0;
+            addButton.Enabled = true;
+            showAllButton.Enabled = true;
+        }
+        
+    }
+}
